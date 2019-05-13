2e7b96db05ee1878514776f606a002b28cd9def3
b00baleh <aluke.oe@gmail.com>
2019-05-13 23:04:22 +0300

    -

---
+++ DynamicArray/DynArray.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamicArray
+{
+    public class DynArray<T>
+    {
+        private T[] _arr;
+        private long _currSize;
+
+        public void Put(int i, T element)
+        {
+            if (_arr == null)
+            {
+                _currSize = i + 100;
+                _arr = new T[_currSize];
+                _arr[i] = element;
+            }
+            else if (i >= _currSize)
+            {
+                var newArr = new T[i + 100];
+                for (var j = 0; j < _arr.Length; j++)
+                {
+                    newArr[j] = _arr[j];
+                }
+                _arr = newArr;
+                _arr[i] = element;
+            }
+            else
+            {
+                _arr[i] = element;
+            }
+        }
+
+        public T Get(int i)
+        {
+            return _arr[i];
+        }
+
+        public long Length()
+        {
+            return _arr.Length;
+        }
+    }
+}
---
+++ DynamicArray/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamicArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var arr = new DynArray<int>();
+            arr.Put(10, 123);
+            Console.WriteLine(arr.Get(10));
+
+            for(var i=0; i< 1000; i++)
+                arr.Put(i,i);
+
+            for (var i = 0; i < arr.Length(); i++)
+                Console.Write(arr.Get(i) + " ");
+            Console.ReadLine();
+        }
+    }
+}
